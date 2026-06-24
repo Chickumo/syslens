87d92e60e729f0d0e8d128e903d1f1ef6cf5c8c9
Your Name <you@example.com>
2026-06-24 23:44:56 +0530

    Initialized Syslens v0.1 with some system info

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,162 @@
-# include <iostream>
-# include <unistd.h>
+#include <iostream>
+#include <iomanip>
+#include <unistd.h>
+#include <sys/sysinfo.h>
+#include <sys/utsname.h>
+#include <ifaddrs.h>
+#include <arpa/inet.h>
+#include <netpacket/packet.h>
+#include <map>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
-int main(int argc, char *argv[]){
+const string separator = "====================================================================";
 
-    cout << "===== SYSLENS =====" << endl;
-    cout << "PID: " << getpid() << endl;
-    cout << "PPID: " << getppid() << endl;
+struct NetworkInterface {
+    string ipv4;
+    string mac;
+};
 
+void systemInfo() {
+    struct sysinfo si;
+    struct utsname u;
+    struct ifaddrs* ifaddr;
+    char hostname[256];
+
+    if (sysinfo(&si) == 0) {cout << right << setw(separator.length()) << "\n                                                  Uptime: " << si.uptime / 3600 << "H " << (si.uptime % 3600) / 60 << "M " << si.uptime % 60 << "S" << endl;}
+    cout << separator << endl;
+    if (uname(&u) == 0) {cout << "\nOS: " << u.sysname << " " << u.release << " " << u.machine << endl;}
+    if (gethostname(hostname, sizeof(hostname)) == 0) {cout << "Hostname : " << hostname << endl;}
+
+    map<string, NetworkInterface> interfaces;
+
+    if (getifaddrs(&ifaddr) == 0) {
+        for (auto* p = ifaddr; p; p = p->ifa_next) {
+            if (!p->ifa_addr)
+                continue;
+
+            string name = p->ifa_name;
+
+            if (p->ifa_addr->sa_family == AF_INET) {
+                char ip[INET_ADDRSTRLEN];
+
+                auto* addr =
+                    (struct sockaddr_in*)p->ifa_addr;
+
+                inet_ntop(AF_INET, &(addr->sin_addr), ip, sizeof(ip));
+
+                interfaces[name].ipv4 = ip;
+            }
+
+            else if (p->ifa_addr->sa_family == AF_PACKET) {
+                auto* s =
+                    (struct sockaddr_ll*)p->ifa_addr;
+                if (s->sll_halen > 0) {
+                    stringstream mac;
+                    for (int i = 0; i < s->sll_halen; i++) {
+                        if (i)
+                            mac << ":";
+                        mac << hex
+                            << setw(2)
+                            << setfill('0')
+                            << (int)s->sll_addr[i];
+                    }
+
+                    interfaces[name].mac = mac.str();
+                }
+            }
+        }
+
+        freeifaddrs(ifaddr);
+    }
+
+    string gateway = "Unknown";
+
+    ifstream route("/proc/net/route");
+
+    if (route) {
+
+        string line;
+        getline(route, line);
+
+        while (getline(route, line)) {
+
+            istringstream iss(line);
+
+            string iface;
+            string destination;
+            string gw;
+
+            iss >> iface >> destination >> gw;
+
+            if (destination == "00000000") {
+
+                unsigned long g = stoul(gw, nullptr, 16);
+                struct in_addr addr;
+                addr.s_addr = g;
+                gateway = inet_ntoa(addr);
+                break;
+
+            }
+        }
+    }
+
+    cout << "\nINTERFACES\n";
+
+    for (const auto& [name, info] : interfaces) {
+
+        if (name == "lo")
+            continue;
+
+        cout << "  " << name << '\n';
+
+        if (!info.ipv4.empty()){cout << "    IPv4 : " << info.ipv4 << '\n';}
+        if (!info.mac.empty()){cout << "    MAC  : " << info.mac << '\n';}
+        cout << '\n';
+    }
+
+    cout << "GATEWAY - " << gateway << endl;
+
+    if (sysinfo(&si) == 0) {
+
+        double usedRam = (double)(si.totalram - si.freeram) * si.mem_unit / (1024.0 * 1024 * 1024);
+        double totalRam = (double)si.totalram * si.mem_unit / (1024.0 * 1024 * 1024);
+
+        cout << "\nRAM: " << fixed << setprecision(2) << usedRam << " GB / " << totalRam << " GB" << endl;
+    }
+}
+
+void displayMenu() {
+
+    cout << "\033[1;36m";
+
+    cout << R"( ███████╗██╗   ██╗███████╗██╗     ███████╗███╗   ██╗███████╗
+ ██╔════╝╚██╗ ██╔╝██╔════╝██║     ██╔════╝████╗  ██║██╔════╝
+ ███████╗ ╚████╔╝ ███████╗██║     █████╗  ██╔██╗ ██║███████╗
+ ╚════██║  ╚██╔╝  ╚════██║██║     ██╔══╝  ██║╚██╗██║╚════██║
+ ███████║   ██║   ███████║███████╗███████╗██║ ╚████║███████║
+ ╚══════╝   ╚═╝   ╚══════╝╚══════╝╚══════╝╚═╝  ╚═══╝╚══════╝
+)" << endl;
+
+    cout << "\033[0m";
+
+    string info =
+        "PID: " + to_string(getpid()) +
+        "\tPPID: " + to_string(getppid()) +
+        "\tShell: " + getenv("SHELL") +
+        "\tUser: " + getenv("USER");
+
+    cout << left
+         << setw(separator.length())
+         << info
+         << endl;
+
+    systemInfo();
+}
+
+int main() {
+    displayMenu();
     return 0;
 }
